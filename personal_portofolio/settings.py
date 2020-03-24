9e82143f168913a50c4e00366bd8939229ddad90
GhitaAlexandruIon <ghita.alexandru6@gmail.com>
2020-03-24 21:33:41 +0200

    Footer links

--- personal_portofolio/settings.py
+++ personal_portofolio/settings.py
@@ -81,8 +81,13 @@ WSGI_APPLICATION = 'personal_portofolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'GhitaAlexandruIo$portofolio',
+        'USER': 'GhitaAlexandruIo',
+        'PASSWORD': 'GreenArrow',
+        'HOST': 'GhitaAlexandruIon.mysql.pythonanywhere-services.com',
+        'PORT': '',
+
     }
 }
 
