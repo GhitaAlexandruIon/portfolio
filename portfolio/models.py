33351a64a0f811c67a0f41680e8c14c0e83f55f0
GhitaAlexandruIon <ghita.alexandru6@gmail.com>
2020-04-04 14:58:17 +0300

    multi select field

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from multiselectfield import MultiSelectField
+
+
+languages = (
+    ('HTML', 'HTML'), ('CSS', 'CSS'), ('JavaScript', 'JavaScript'),
+    ('Bootstrap', 'Bootstrap'), ('jQuery', 'jQuery'), ('Python', 'Python')
+
+)
 
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = RichTextField()
+    programing_languages = MultiSelectField(choices=languages)
     image = models.ImageField(upload_to='portfolio/images')
     url = models.URLField(blank=True)
 
